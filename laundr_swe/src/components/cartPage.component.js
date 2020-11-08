01cf0decdf8adf8ca26e016ecb7325397b0c75df
Jared Herbert <jaredherberttsa@gmail.com>
2020-11-08 17:28:35 -0500

    Fixed css files and linked cart

--- laundr_swe/src/components/cartPage.component.js
+++ laundr_swe/src/components/cartPage.component.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'semantic-ui-css/semantic.min.css';
+//import 'semantic-ui-css/semantic.min.css';
 import {updateItem} from '../controllers/cartFunctions';
 import {useStatelocal} from '../controllers/cartFunctions';
 import coffeeImg from "../laundrassets/mockups/coffeeMockup.png";
