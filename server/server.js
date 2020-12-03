b0e25d9303ded1d940f1cd7c5d36c00904488778
Jared Herbert <jaredherberttsa@gmail.com>
2020-12-03 14:39:49 -0500

    try 7

--- server/server.js
+++ server/server.js
@@ -31,13 +31,21 @@ app.use(bodyParser.json());
 app.use('/api/laundrProducts/', laundrProductRouter);
 
 app.all('/*', (req, res) => {
-
-   res.statusCode === 404 ? res.send('Sorry, information not available') : res.sendFile(path.resolve('./client/index.html'))
-        
+   res.statusCode === 404 ? res.send('Sorry, information not available') : res.sendFile(path.resolve('./client/index.html'))   
 });
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('../build'));
+}
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../../client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+  });
 }
 
 const port = process.env.PORT || config.port;
