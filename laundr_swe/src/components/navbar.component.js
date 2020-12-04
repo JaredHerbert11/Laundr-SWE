143b8912df18ad239b1cb7ec9e3e6233972e0057
Valentina Casteline <valentinacasteline@ssrb-vpn2-13-95.vpn.ufl.edu>
2020-12-04 18:00:26 -0500

    animations and navbar for mobile

--- laundr_swe/src/components/navbar.component.js
+++ laundr_swe/src/components/navbar.component.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 //import Navbar from 'react-bootstrap/Navbar'
 import '../css/navbar.css'
 import Grid from '@material-ui/core/Grid';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 import logo from '../laundrassets/logos/logo2.png'
 import {Link} from 'react-router-dom';
+import Drawer from '@material-ui/core/Drawer';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 const NavBar = () => {
+    const [open, setOpen] = React.useState(false);
+    const theme = useTheme();
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+    var drawerHeader= {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-start',
+    }
     return(
         <div className='Navbar'>
             <Grid container spacing={3}>
@@ -20,7 +41,17 @@ const NavBar = () => {
                         <div className="nav">
                             <Link to="/">Home </Link>
                             <Link to="/cart" ><ShoppingCartIcon color="black"  /></Link>
+                           
                         </div>
+                        <MenuIcon id="menu" onClick={handleDrawerOpen}></MenuIcon>
+                        <div className="sidenav">
+                            <Drawer anchor='right' open={open} variant="persistent" >
+                                <div className={drawerHeader}><CloseIcon onClick={handleDrawerClose}></CloseIcon></div>
+                                <Link style={{padding:"1.5em", marginRight:"1.5em"}} to="/">Home </Link>
+                                <Link style={{paddingLeft:"1.5em"}} to="/cart" > Cart</Link>
+                            </Drawer>
+                        </div>
+                        
                 </Grid>
             </Grid>
         </div>
--- laundr_swe/src/components/productpage-gardenia.js
+++ laundr_swe/src/components/productpage-gardenia.js
@@ -1,11 +1,51 @@
+import React, { useEffect } from 'react'
+//import { useScrollPosition } from '@n8tb1t/use-scroll-position'
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import gardenia from "../laundrassets/mockups/gardeniaMockup.png"
 import "../css/productpage.css"
+import bomb from "../laundrassets/bombs/Laundr\ Bomb\ Angle1.png"
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
+gsap.core.globals('ScrollTrigger', ScrollTrigger);
 
 const Gardenia = (props) =>{
+
+    useEffect(() => {
+ 
+        gsap.to(".bomb", {
+            scrollTrigger: {
+                trigger:".gardenia",
+                start: 1,
+                end:"100%",
+                scrub:true
+            },
+            x: 400,
+            pin:true,
+            rotation: 360,
+            ease:"none",
+            duration:3
+            
+        });
+        gsap.to(".bag1", {
+            scrollTrigger: {
+                trigger:".product",
+                start: 1,
+                end:"100%",
+                scrub:true
+            },
+            y: window.innerHeight,
+            x:-window.innerWidth,
+            
+            ease:"none",
+            
+        });
+ 
+    }, []);
     return (
-        <div className="gardenia">
+        <div className="product">
+            <div className="gardenia">
                         
             <Grid container spacing={2}>
                 <Grid item xs sm={6}>
@@ -19,23 +59,29 @@ const Gardenia = (props) =>{
                     </div>
                 </Grid> 
             </Grid>
-            <img src={gardenia} className="bag1"></img>
-                <div className="description">
-                    <div className="textBlock prod-des">
+            <div className="bomb"><img src={bomb}></img></div>
+            <div className="picture" id="bagId"><img src={gardenia} className="bag1"></img></div>
+            
+         </div>
+         <div >
+            <Grid container spacing={2} className="description">
+                    <Grid item xs sm={6} className="textBlock prod-des">
                         <h2>Product Description</h2>
                         <p>21 count bag of “White Gardenia” scented Laundr Bombs<br/>
                         A sweet and floral perfume with relaxing notes, best used after a <br/>
                         carefree frolic through a field of wildflowers
                         </p>
-                    </div>
-                    <div className="textBlock">
+                    </Grid>
+                    <Grid item xs sm={6} className="textBlock">
                         <h2>Scent</h2>
                         <p>This fragrance has hints of: </p>
                         <li>Gardenia Shrub</li>
                         <li>Floral Essence</li>
                         <li>Apple Water</li>
-                    </div> 
-                </div>
+                    </Grid> 
+            </Grid>
+
+         </div>
         </div>
     )
 }
