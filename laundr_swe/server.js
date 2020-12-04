d0be92ac7dd124d491cb64640e50c3a9b9b23773
Ammar Abdullah <ammarabdullah0466@gmail.com>
2020-12-04 14:29:31 -0500

    Finished Stripe Draft
    Had to change start script in package.json.
    Needed to add some dependencies in package.json

---
+++ laundr_swe/server.js
@@ -0,0 +1,38 @@
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+const express = require('express');
+const app = express();
+app.use(express.static('.'));
+
+const YOUR_DOMAIN = 'http://localhost:3000/cart';
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.post('/create-session', async (req, res) => {
+  let item = [];
+  for (let i = 0; i < req.body.length; i++){
+    let temp = {
+      price_data: {
+        currency: 'usd',
+        product_data: {
+          name: req.body[i].id,
+          images: ['https://i.imgur.com/y0Xntmz.jpg'],
+        },
+        unit_amount: req.body[i].price,
+      },
+      quantity: req.body[i].quantity,
+    }
+    item.push(temp);
+  }
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: item,
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+  });
+
+  res.json({ id: session.id });
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
--- laundr_swe/src/components/cartPage.component.js
+++ laundr_swe/src/components/cartPage.component.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { loadStripe } from "@stripe/stripe-js";
 import {updateItem} from '../controllers/cartFunctions';
 import {useStatelocal} from '../controllers/cartFunctions';
 import coffeeImg from "../laundrassets/mockups/coffeeMockup.png";
@@ -9,6 +10,7 @@ import gardeniaImg from "../laundrassets/mockups/gardeniaMockup.png";
 import teakwoodImg from "../laundrassets/mockups/mahoganymockup.png";
 import CartItem from "./cartItem.component";
 import '../css/cartpage.css';
+const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
 
 
 //replace hard coded entries with CRUD calls
@@ -16,37 +18,37 @@ let Coffee = {
     id : "Coffee Vanilla",
     quantity : 0,
     picture:coffeeImg,
-    price:18.99
+    price:1899
 }
 let TeaTree = {
     id : "Eucalyptus Tea Tree",
     quantity : 0,
     picture:teaTreeImg,
-    price:18.99
+    price:1899
 }
 let Air = {
     id : "Fresh Air",
     quantity : 0,
     picture:freshAirImg,
-    price:18.99
+    price:1899
 }
 let Watermelon = {
     id : "Watermelon Cucumber",
     quantity : 0,
     picture:watermelonImg,
-    price:18.99
+    price:1899
 }
 let Gardenia = {
     id : "White Gardenia",
     quantity : 0,
     picture:gardeniaImg,
-    price:18.99
+    price:1899
 }
 let Teakwood = {
     id : "Mahogany Teakwood",
     quantity : 0,
     picture:teakwoodImg,
-    price:18.99
+    price:1899
 }
 
 
@@ -54,6 +56,7 @@ const CartPage = (props) => {
     let [cart, setCart] = useStatelocal();
     let cartItems = [];
     let totalPrice = 0;
+
     //iterate over each item in the cart to calculate price and add CartItem component to array in order to be returned w the page
     for(let i = 0; i < cart.length;i++){
         let item = {
@@ -67,7 +70,7 @@ const CartPage = (props) => {
             item.quantity=cart[i].quantity;
             //replace object population w crud calls
             item.picture = cart[i].picture;
-            item.price = cart[i].price;
+            item.price = cart[i].price / 100;
             totalPrice += item.price*item.quantity;
         }
 
@@ -76,6 +79,42 @@ const CartPage = (props) => {
         );
     }
 
+    const handleClick = async (event) => {
+        const stripe = await stripePromise;
+        let cartString = JSON.stringify(cart);
+        const response = await fetch("/create-session", {
+          method: "POST",
+          body: cartString,
+          headers: {
+            "Content-Type": "application/json",
+          }
+        });
+
+        const session = await response.json();
+    
+        // When the customer clicks on the button, redirect them to Checkout.
+        const result = await stripe.redirectToCheckout({
+          sessionId: session.id,
+        });
+    
+        if (result.error) {
+          // If `redirectToCheckout` fails due to a browser or network
+          // error, display the localized error message to your customer
+          // using `result.error.message`.
+        }
+    };
+
+    useEffect(() => {
+        // Check to see if this is a redirect back from Checkout
+        const query = new URLSearchParams(window.location.search);
+        if (query.get("success")) {
+          console.log("Order was a success!")
+        }
+    
+        if (query.get("canceled")) {
+            console.log("Order was cancelled, when did god forsaken us?");
+        }
+      }, []);
 
     return (
         <div className="bg">
@@ -83,7 +122,7 @@ const CartPage = (props) => {
                 <div class="header">
                     <div className="top">
                         <span> Your Cart </span>
-                        <span> Proceed to Checkout </span>
+                        <p className="checkout" onClick={handleClick}> Proceed to Checkout </p>
                     </div>
                 </div>
                     <table class="ui celled collapsing very basic table" style={{minWidth : "700px"}}>
